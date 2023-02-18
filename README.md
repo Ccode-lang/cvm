e1cfd3ee68754ab8292e8f13bc415765078d0be5
BuildTools <unconfigured@null.spigotmc.org>
2023-02-18 12:44:42 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# CVM (Ccode's virtual machine)
+This repo contains a assembly like language that is really cursed.
+
+# The idea
+I am making an assembly like language that only uses high level types and is interpreted instead of compiled.
---
+++ cvm.py
@@ -0,0 +1,61 @@
+import sys
+memory = {}
+instructions = []
+instcount = 0
+
+f = open("test.cvm", "r")
+instructions = f.readlines()
+f.close()
+
+retaddr = 0
+
+def setMemAddr(addr, value):
+    memory[int(addr)] = value
+while True:
+    try:
+        inst = instructions[instcount].strip()
+    except:
+        sys.exit()
+    if inst.split(" ")[0] == "set":
+        if inst.split(" ")[2].isnumeric():
+            setMemAddr(inst.split(" ")[1], int(inst.split(" ")[2]))
+        else:
+            setMemAddr(inst.split(" ")[1], inst.split(" ", 2)[2])
+    elif inst.split(" ")[0] == "add":
+        setMemAddr(inst.split(" ")[1], memory[int(inst.split(" ")[2])] + memory[int(inst.split(" ")[3])])
+    elif inst.split(" ")[0] == "dis":
+        try:
+            display = memory[int(inst.split(" ")[1])]
+        except:
+            display = ""
+        print(display)
+    elif inst == "":
+        pass
+    elif inst.split(" ")[0] == "jmp":
+        counter = 0
+        while True:
+            line = instructions[counter].strip()
+            if line == inst.split(" ", 1)[1]:
+                instcount = counter
+                break
+            counter += 1
+    elif inst.split(" ")[0] == "call":
+        counter = 0
+        retaddr = instcount
+        while True:
+            line = instructions[counter].strip()
+            if line == inst.split(" ", 1)[1]:
+                instcount = counter
+                break
+            counter += 1
+    elif inst == "ret":
+        instcount = retaddr
+    elif inst.startswith("<") and inst.endswith(">"):
+        pass
+    elif inst.startswith(";"):
+        pass
+    else:
+        print("Unknown instruction")
+        print(inst)
+        sys.exit()
+    instcount += 1
